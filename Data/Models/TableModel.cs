3ca7e9177791824620a222c12e94581633130d78
Nicolas Teichtweier <Nicolas.Teichtweier@inpro.de>
2023-10-29 18:12:52 +0100

    moved from tauri to blazor

---
+++ Data/Models/TableModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TBK.Certs.Data.Models;
+
+public class TableModel
+{
+    public string Email { get; set; }
+    public string Name { get; set; }
+    public string Birthday { get; set; }
+}
